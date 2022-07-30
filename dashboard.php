b6f64d6fcd3b5959ca3fb23ad9b72725b436fd22
Yosua Christianov <yosua.christianov@gmail.com>
2022-07-30 16:41:42 +0700

    v.0.2.10 - summary

--- dashboard.php
+++ dashboard.php
@@ -40,6 +40,85 @@
 						<button type="submit"  onclick="fade()" class="btn btn-success tp-rnd-20"><i class="fa-brands fa-whatsapp"></i> Kirim Sekarang ke Whatsapp</button>
 					</form>
 
+					<div class="col-12 card tp-bt-cs-blue">
+						<div class="row">
+							<div class="col-12">
+								<table class="table table-striped table-dark">
+									<thead>
+										<tr>
+											<td class="tp-bc-white tp-fc-black" colspan="3"><span class="tp-fs-bold tp-fs-italic">Statistik</span></td>
+										</tr>
+										<tr>
+											<th>Total Pendaftar</th>
+											<td>23</td>
+											<td>Peserta</td>
+										</tr>
+										<tr>
+											<td class="tp-bc-white tp-fc-black" colspan="3"><span class="tp-fs-bold tp-fs-italic">Berdasarkan Gender</span></td>
+										</tr>
+										<?php 
+											$resStat = queryBack("SELECT mg.name,count(*) as summary FROM form_attendee fa JOIN mst_gender mg on mg.id=fa.gender WHERE fm='".$form[$i]."' GROUP BY mg.name");
+											for ($j=0; $j < $resStat[0]; $j++) { 
+										?>
+												<tr>
+													<th><?php echo $resStat[1][$j]['name'] ?></th>
+													<td><?php echo $resStat[1][$j]['summary'] ?></td>
+													<td>Peserta</td>
+												</tr>
+										<?php
+											}
+										?>
+										<tr>
+											<td class="tp-bc-white tp-fc-black" colspan="3"><span class="tp-fs-bold tp-fs-italic">Kelas SYM</span></td>
+										</tr>
+										<?php 
+											$resStat = queryBack("SELECT ms.name,count(*) as summary FROM form_attendee fa JOIN mst_sym ms on ms.id=fa.sym_class WHERE fm='".$form[$i]."' GROUP BY ms.name");
+											for ($j=0; $j < $resStat[0]; $j++) { 
+										?>
+												<tr>
+													<th><?php echo $resStat[1][$j]['name'] ?></th>
+													<td><?php echo $resStat[1][$j]['summary'] ?></td>
+													<td>Peserta</td>
+												</tr>
+										<?php
+											}
+										?>
+										<tr>
+											<td class="tp-bc-white tp-fc-black" colspan="3"><span class="tp-fs-bold tp-fs-italic">Bahasa Roh</span></td>
+										</tr>
+										<?php 
+											$resStat = queryBack("SELECT mbr.name,count(*) as summary FROM form_attendee fa JOIN mst_bahasa_roh mbr on mbr.id=fa.method WHERE fm='".$form[$i]."' GROUP BY mbr.name");
+											for ($j=0; $j < $resStat[0]; $j++) { 
+										?>
+												<tr>
+													<th><?php echo $resStat[1][$j]['name'] ?></th>
+													<td><?php echo $resStat[1][$j]['summary'] ?></td>
+													<td>Peserta</td>
+												</tr>
+										<?php
+											}
+										?>
+										<tr>
+											<td class="tp-bc-white tp-fc-black" colspan="3"><span class="tp-fs-bold tp-fs-italic">Domisili</span></td>
+										</tr>
+										
+										<?php 
+											$resStat = queryBack("SELECT city,count(*) AS summary FROM form_attendee WHERE fm='".$form[$i]."' GROUP BY city");
+											for ($j=0; $j < $resStat[0]; $j++) { 
+										?>
+												<tr>
+													<th><?php echo $resStat[1][$j]['city'] ?></th>
+													<td><?php echo $resStat[1][$j]['summary'] ?></td>
+													<td>Peserta</td>
+												</tr>
+										<?php
+											}
+										?>
+									</thead>
+								</table>
+							</div>
+						</div> 
+					</div>
 					<?php 
 						for ($j=0; $j < $res[0]; $j++) { 
 							$fixClassSYM = $res[1][$j]['sym_class'];
@@ -50,11 +129,6 @@
 							}
 
 							$fixHOW = '';
-							if ($res[1][$j]['method'] == 0) {
-								$fixHOW = 'Sudah Bisa';
-							}else{
-								$fixHOW = 'Belum Bisa';
-							}
 					?>
 					<div class="col-12 card tp-bt-cs-blue">
 						<div class="row">
@@ -70,11 +144,41 @@
 								DOB : <?php echo $res[1][$j]['date_birth'] ?>/<?php echo $res[1][$j]['month_birth'] ?>/<?php echo $res[1][$j]['year_birth'] ?><br>
 								<hr class="tp-hr">
 								Kelas SYM yang diikuti : <?php echo $fixClassSYM ?> <br>
-								Bahasa Roh : <?php echo $fixHOW ?>
+								
 								<?php if ($form[$i] == 'fm-x1'): ?>
 									<div>
 										<button class="btn btn-danger tp-rnd-20" onclick="goToPage('exporter/exporter.php?p=<?php echo encrypt_decrypt_ori('encrypt',$res[1][$j]['id']) ?>')"><i class="fa-duotone fa-file-pdf"></i> Export PDF</button>
 									</div>
+									<?php 
+										if ($res[1][$j]['method'] == 0) {
+											$fixHOW = 'Offline';
+										}else{
+											$fixHOW = 'Online';
+										}
+									?>
+									Cara Ikut Pentahiran : <?php echo $fixHOW ?>
+								<?php endif ?>
+
+								<?php if ($form[$i] == 'fm-x2'): ?>
+									<?php 
+										if ($res[1][$j]['method'] == 0) {
+											$fixHOW = 'Sudah Bisa';
+										}else{
+											$fixHOW = 'Belum Bisa';
+										}
+									?>
+									Bahasa Roh : <?php echo $fixHOW ?> <br>
+
+									<?php 
+										if ($res[1][$j]['gender'] == 1) {
+											$gender = 'Pria';
+										}else if ($res[1][$j]['gender'] == 2) {
+											$gender = 'Wanita';
+										}else{
+											$gender = 'Tidak disebutkan';
+										}
+									?>
+									Gender : <?php echo $gender ?>
 								<?php endif ?>
 							</div>
 						</div>
